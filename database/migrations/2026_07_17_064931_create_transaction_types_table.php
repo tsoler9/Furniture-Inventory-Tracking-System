45aa8b91b01f7e4b1a969af1cc7ff716dacc5ea7
Samuel Magana <maganasamuel001@gmail.com>
2026-07-24 14:14:18 +0800

    Add mode column to transation types table

--- database/migrations/2026_07_17_064931_create_transaction_types_table.php
+++ database/migrations/2026_07_17_064931_create_transaction_types_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('transaction_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('mode');
             $table->string('description')->nullable();
             $table->timestamps();
         });
