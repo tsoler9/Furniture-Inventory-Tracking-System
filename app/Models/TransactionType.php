1ab2f2aa84791c959c6b638dcc290e7eeddd0c19
Samuel Magana <maganasamuel001@gmail.com>
2026-07-24 14:15:19 +0800

    Use empty array guarded instead of fillable

--- app/Models/TransactionType.php
+++ app/Models/TransactionType.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TransactionType extends Model
 {
-    protected $fillable = ['name', 'description'];
+    use HasFactory;
+
+    protected $guarded = [];
 
     public function transactions()
     {
